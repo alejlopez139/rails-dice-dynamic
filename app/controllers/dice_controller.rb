25e1f1190115a2bb7883f8e29a5726aac8b5b4a4
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-26 16:46:46 +0000

    route for 2d10 works

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -9,8 +9,17 @@ class DiceController < ApplicationController
     die = rand(1..6)
 
     @rolls.push(die)
-  end
+    end
     render ({:template => "dice_templates/2d6"})
   end
-  
+  def two_ten
+    @rolls = []
+
+    2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+    end
+    render ({:template => "dice_templates/two_ten"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/" , {:controller => "dice", :action =>"home"})
   get("/dice/2/6", {:controller => "dice", :action => "twodsix"})
+  get("/dice/2/10", {:controller => "dice", :action => "two_ten"})
 end
