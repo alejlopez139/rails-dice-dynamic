1cc146b72a405fda28ded5c8493921ba89c5e827
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-26 17:19:45 +0000

    finished all routes

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -42,5 +42,19 @@ class DiceController < ApplicationController
     end
     render ({:template => "dice_templates/five_four"})
   end
+  def dynamic
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+    render ({:template => "dice_templates/flexible"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get("/dice/2/10", {:controller => "dice", :action => "two_ten"})
   get("/dice/1/20", {:controller => "dice", :action => "one_twenty"})
   get("/dice/5/4", {:controller => "dice", :action => "five_four"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "dynamic"})
 end
