bd1893e8ea1994e7ece478411437fef14924e3c7
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-26 16:41:02 +0000

    2d6 route works

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,6 +3,14 @@ class DiceController < ApplicationController
     render({:template => "dice_templates/home"})
   end
   def twodsix
+    @rolls = []
+
+    2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
     render ({:template => "dice_templates/2d6"})
   end
+  
 end
